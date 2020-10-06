a3a7305f2159212fc099d6b3ff2977dffcf1bbe9
Stanislav Alimpov <stanislav.alimpov@vrpconsulting.com>
2020-10-06 18:08:41 +0300

    feat: implemented code

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    // for(let item in MORSE_TABLE) {
+    //     let itemNewValue = item.split('').map((symbol) => {
+    //         return symbol === '-' ? '11' : '10';
+    //     }).join('');
+    //     if (itemNewValue.length < 10) {
+    //         itemNewValue = '0'.repeat(10 - itemNewValue.length) + itemNewValue;
+    //     }
+    //     MORSE_TABLE[itemNewValue] = MORSE_TABLE[item];
+    // }
+    let result = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        if(expr[i] === '*') {
+            result += ' ';
+        } else {
+           let symb = expr.substr(i, 10);
+           let letterCode = '';
+           for (let i = 0; i < symb.length; i += 2) {
+               if (symb[i] + symb[i + 1] == '10') {
+                letterCode += '.';
+               }
+               if (symb[i] + symb[i + 1] == '11') {
+                letterCode += '-';
+               }
+           }
+            result += MORSE_TABLE[letterCode];
+        }
+    }
+    return result;
+    // expr.split('').map(item => {
+    //     console.log(expr);
+    //     console.log(item);
+    //     if (item === ' ') {
+    //         return '**********';
+    //     } else {
+    //         return this.MORSE_TABLE[item]
+    //     }
+    // }).join('');
 }
 
 module.exports = {
